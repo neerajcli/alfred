4cb3e43a8305f1cac7d6e3e21a83de792ff101dc
neerajcli <128034389+neerajcli@users.noreply.github.com>
2026-03-02 18:35:44 +0530

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -4,12 +4,19 @@ description: "Give feedback for our bot.",
 category: "Other",
 execute: async (client, message, args) => {
     if (message.author.bot) return;
-    const discord = require('discord.js')
-    const embed = new discord.MessageEmbed()
-    .setTitle('Feedback')
-    .setDescription(`[Click Here](https://tinyurl.com/alfredbotfeedback) to give feedback for our bot.`)
+    const targetChannelID = '1478012706731851847';
+    const targetChannel = client.channels.cache.get(targetChannelID);
+    if (!targetChannel) return message.channel.send('A critical error occured. Please contact developers.'); 
+    let feedback = args.slice(0).join(" ");
+    if(!feedback) return message.channel.send("Please specify feedback message!");
+    const Discord = require('discord.js');
+    const embed = new Discord.MessageEmbed()
     .setColor('#FFFFFF')
+    .setTitle('Feedback')
+    .setDescription(feedback)
     .setTimestamp()
-    message.channel.send(embed)
+    .setFooter(`Sent by ${message.author.username} (${message.author.id})`)
+    targetChannel.send(embed)
+    message.channel.send("Your feedback has been submitted!")
 }
 }
